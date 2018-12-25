c0a86b90f00cd2d4edbe84bde2cd79df76ec2755
Alexey <kalinichenkoae@gmail.com>
2018-12-25 08:52:15 +0300

    remove virtual enviroment

--- virt/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/alexey/Documents/reps/Game project/django_test/virt/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
